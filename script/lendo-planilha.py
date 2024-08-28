b8dd5951659219457c510116c8518b3a20269de4
Mayara Munis <102186483+Mmunis17@users.noreply.github.com>
2024-08-28 19:03:44 -0300

    Update lendo-planilha.py
    Versão final do código - 28/08/2024

--- script/lendo-planilha.py
+++ script/lendo-planilha.py
@@ -3,6 +3,7 @@ import pyautogui
 import time
 import os
 
+
 pyautogui.FAILSAFE = False  # Desativa o fail-safe
 
 # Salvar a posição inicial do cursor
@@ -15,22 +16,23 @@ caminho_pasta = 'C:\\Robo\\arquivos-pedidos\\'
 arquivos_excel = [f for f in os.listdir(caminho_pasta) if f.endswith('.xlsx')]
 
 for arquivo in arquivos_excel:
+    print(f"Iniciando o processamento da planilha: {arquivo}")  # Informar a planilha que está sendo processada
     caminho_arquivo = os.path.join(caminho_pasta, arquivo)
     df = pd.read_excel(caminho_arquivo, usecols=[0])  # Lê apenas a primeira coluna
 
     # Converte a coluna em uma lista de strings (removendo valores nulos)
     linhas = df.iloc[:, 0].dropna().tolist()
 
-    # Passo 1: Selecionar a opção da tecla F2
+# Passo 1: Selecionar a opção da tecla F2
     pyautogui.click(x=20, y=187) 
     time.sleep(5)  # Pequena pausa para garantir o processamento
 
-    # Passo 2: Informar cliente e filial
+# Passo 2: Informar cliente e filial
     pyautogui.click(x=114, y=167)  # Substitua (x, y) com as coordenadas reais do campo Cliente/Filial
-    pyautogui.write('006 0')  # Informar cliente e filial
+    pyautogui.write('007 0')  # Informar cliente e filial
     time.sleep(2)
 
-    # Passo 3: Selecionar a natureza da operação
+# Passo 3: Selecionar a natureza da operação
     pyautogui.click(x=296, y=465)  # Clicar no campo Natureza da Operação
     time.sleep(1)
 
@@ -42,6 +44,7 @@ for arquivo in arquivos_excel:
     # operação
     pyautogui.click(x=150, y=250)  # Substitua (x, y) com as coordenadas reais do campo Natureza da Operação
     time.sleep(0.05)
+    
     # Digitar o '5927-03' com intervalo mais rápido entre as teclas
     pyautogui.click(x=229, y=459)
  
@@ -53,40 +56,38 @@ for arquivo in arquivos_excel:
     # Pressionar 'Enter' para confirmar a seleção
     pyautogui.press('enter')
 
-    # Passo 4: Clicar em avançar
+# Passo 4: Clicar em avançar
     pyautogui.click(x=768, y=70)  # Substitua (x, y) com as coordenadas reais do botão Avançar
     time.sleep(1)
 
-    # Passo 5: Inserir produtos da planilha
+# Passo 5: Inserir produtos item a item na planilha
     for linha in linhas:
         pyautogui.click(x=491, y=57)  # Substitua (x, y) com as coordenadas reais do campo de inserção do produto
         pyautogui.write(str(linha))  # Digita o produto atual
         pyautogui.press('enter')  # Confirma o produto e passa para o próximo
         time.sleep(0.5)  # Pequena pausa para garantir que o sistema tenha tempo de processar
 
-    # Passo 6: Finalizar com F12
+# Passo 6: Finalizar F12 - processo de emissão
     pyautogui.press('F12')
     time.sleep(2)
 
-    # Passo 6: Finalizar com F12 - ajustar
-    pyautogui.click(x=299, y=143)
+    pyautogui.click(x=299, y=143) #possicionar e dar esc
     pyautogui.press('Esc')
     time.sleep(2)
     
-   
-    pyautogui.click(x=448, y=346) # Substitua (x, y) com as coordenadas reais do campo Cliente/Filial
-    pyautogui.write('xxxxxx')  # Informar senha
+    pyautogui.click(x=448, y=346)  # Substitua (x, y) com as coordenadas reais do campo Cliente/Filial
+    pyautogui.write('361713')  # Preencher senha
     time.sleep(2)
     
-    # Passo 6: Finalizar com F12
     pyautogui.click(x=635, y=374)
-    time.sleep(20)
+    time.sleep(30)
+
+# Fim do processo do robo em tela
 
     # Imprimir mensagem após finalizar cada planilha
     print(f'Finalizado o processamento da planilha: {arquivo}')
 
-    # Voltar o cursor para a posição inicial1*2,2634$11195797
-    
+    # Voltar o cursor para a posição inicial
     pyautogui.moveTo(posicao_inicial)
 
 # Imprimir mensagem após finalizar todas as planilhas
@@ -94,3 +95,6 @@ print('Processo completo para todas as planilhas.')
 
 # Voltar o cursor para a posição inicial ao final
 pyautogui.moveTo(posicao_inicial)
+
+### -- Script Criado em 22/08/2024 -- ### -->MaYaRa<--
+### -- Script Atualizando em 28/08/2024 -- ### -->MaYaRa<--
